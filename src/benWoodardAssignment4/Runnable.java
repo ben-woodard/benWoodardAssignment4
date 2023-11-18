3879bde8c089d0fba038060e00fd94ae5694bc6c
Ben Woodard <benjaminwoodard93@gmail.com>
2023-11-18 10:19:05 -0500

    Update Runnable.java

--- src/benWoodardAssignment4/Runnable.java
+++ src/benWoodardAssignment4/Runnable.java
@@ -5,9 +5,9 @@ public class Runnable {
 	public static void main(String[] args) {
 		FileService fileService = new FileService();
 		Student[] joe = fileService.readStudentFile("student-master-list.csv");
-		Student[] dumb = fileService.organizeStudentsByGrade(joe);
-		for(Student poop : dumb) {
-			System.out.println(poop.getInfo());
+		Student[] allen = fileService.organizeStudentsByGrade(joe);
+		for(Student student : allen) {
+			System.out.println(student.getInfo());
 		}
 	}
 
